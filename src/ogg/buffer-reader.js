a53ab901a41744f4d0ce4a501b82595558632a9d
Adrian Rocke <business@adrianrocke.com>
2018-08-23 17:45:32 -0500

    finished buffer reader

--- src/ogg/buffer-reader.js
+++ src/ogg/buffer-reader.js
@@ -1,20 +1,32 @@
-const create = (source) => {
-  let view;
-  if (source instanceof ArrayBuffer) {
-    view = new DataView(source);
+export default class BufferReader {
+  constructor(source) {
+    if (source instanceof ArrayBuffer) {
+      this._view = new DataView(source);
+    }
+    else if (source instanceof DataView) {
+      this._view = source;
+    }
+    else {
+      throw new TypeError("'source' must be an ArrayBuffer or DataView.");
+    }
+    this._bytePos = 0;
   }
-  else if (source instanceof DataView) {
-    view = source;
-  }
-  else {
-    throw new TypeError("'source' must be an ArrayBuffer or DataView.");
-  }
-  return {
-    view,
-    bytePos: 0
-  };
-};
 
-export default {
-  create
+  get bytePos() {
+    return this._bytePos;
+  }
+
+  get hasNext() {
+    return this._bytePos < this._view.byteLength;
+  }
+
+  readByte() {
+    return this._view.getUint8(this._bytePos++);
+  }
+
+  readDataView(byteLength) {
+    const view = new DataView(this._view.buffer, this._bytePos, byteLength);
+    this._bytePos += byteLength;
+    return view;
+  }
 }
--- tests/unit/ogg/buffer-reader.spec.js
+++ tests/unit/ogg/buffer-reader.spec.js
@@ -1,30 +1,96 @@
 import BufferReader from "@/ogg/buffer-reader";
 
 describe("BufferReader", () => {
-  describe("create", () => {
-    it("sets the bytePos to 0.", () => {
-      const buffer = new ArrayBuffer(1);
-      const reader = BufferReader.create(buffer);
-      expect(reader).toHaveProperty("bytePos", 0);
-    });
-
-    it("creates a DataView for the buffer.", () => {
-      const buffer = new ArrayBuffer(1);
-      const reader = BufferReader.create(buffer);
-      expect(reader).toHaveProperty("view");
-      expect(reader.view.buffer).toBe(buffer);
-      expect(reader.view.byteOffset).toEqual(0);
-    });
-
-    it("uses the given DataView.", () => {
-      const buffer = new ArrayBuffer(10);
-      const view = new DataView(buffer, 1, 5);
-      const reader = BufferReader.create(view);
-      expect(reader).toHaveProperty("view", view);
-    });
-
+  describe("constructor", () => {
     it("throws exception if argument is not ArrayBuffer or DataView.", () => {
-      expect(() => BufferReader.create("")).toThrowError(TypeError);
+      expect(() => new BufferReader("")).toThrowError(TypeError);
+    });
+  });
+
+  describe("hasNext", () => {
+    it("returns true if there are more bytes to be read.", () => {
+      const buffer = new ArrayBuffer(2);
+      const reader = new BufferReader(buffer);
+      expect(reader.hasNext).toEqual(true);
+    });
+
+    it("returns false if there are no more bytes to be read.", () => {
+      const buffer = new ArrayBuffer(0);
+      const reader = new BufferReader(buffer);
+      expect(reader.hasNext).toEqual(false);
+    });
+  });
+
+  describe("readByte", () => {
+    let reader, view;
+
+    beforeEach(() => {
+      const size = 10;
+      const buffer = new ArrayBuffer(size);
+      view = new DataView(buffer);
+      for (let i = 0; i < size; i++) {
+        view.setUint8(i, i);
+      }
+      reader = new BufferReader(view);
+    });
+
+    it("returns the next byte in the view.", () => {
+      for (let i = 0; i < view.byteLength; i++) {
+        const byte = reader.readByte();
+        expect(byte).toEqual(i);
+      }
+    });
+
+    it("moves to the next byte in the view.", () => {
+      for (let i = 0; i < view.byteLength; i++) {
+        reader.readByte();
+        expect(reader.bytePos).toEqual(i + 1);
+      }
+    });
+
+    it("throws exception if the reader is at the end of the view.", () => {
+      for (let i = 0; i < view.byteLength; i++) {
+        reader.readByte();
+      }
+      expect(() => reader.readByte()).toThrowError(RangeError);
+    });
+  });
+
+  describe("readDataView", () => {
+    let reader, view;
+
+    beforeEach(() => {
+      const size = 10;
+      const buffer = new ArrayBuffer(size);
+      view = new DataView(buffer);
+      for (let i = 0; i < size; i++) {
+        view.setUint8(i, i);
+      }
+      reader = new BufferReader(view);
+    });
+
+    it("returns a DataView of the next several bytes in the view.", () => {
+      for (let i = 0; i < view.byteLength / 2; i++) {
+        const view = reader.readDataView(2);
+        expect(view).toBeInstanceOf(DataView);
+        expect(view.buffer).toBe(view.buffer);
+        expect(view.byteOffset).toBe(i * 2);
+        expect(view.byteLength).toBe(2);
+      }
+    });
+
+    it("moves to the next byte in the view.", () => {
+      for (let i = 0; i < view.byteLength / 2; i++) {
+        reader.readDataView(2);
+        expect(reader.bytePos).toEqual(2 * (i + 1));
+      }
+    });
+
+    it("throws exception if the reader is at the end of the view.", () => {
+      for (let i = 0; i < view.byteLength - 1; i++) {
+        reader.readByte();
+      }
+      expect(() => reader.readDataView(2)).toThrowError(RangeError);
     });
   });
 });
