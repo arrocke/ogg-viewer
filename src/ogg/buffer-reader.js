1a961619fb048ce3062ce4310fe0a8ece25afe49
Adrian Rocke <business@adrianrocke.com>
2018-08-23 17:50:12 -0500

    buffer reader comments

--- src/ogg/buffer-reader.js
+++ src/ogg/buffer-reader.js
@@ -1,3 +1,7 @@
+/**
+ * Sequentially reads an ArrayBuffer
+ * @param {ArrayBuffer|DataView} source - The buffer to read.
+ */
 export default class BufferReader {
   constructor(source) {
     if (source instanceof ArrayBuffer) {
@@ -12,18 +16,31 @@ export default class BufferReader {
     this._bytePos = 0;
   }
 
+  /**
+   * Gets the position of the reader in the buffer in bytes.
+   */
   get bytePos() {
     return this._bytePos;
   }
 
+  /**
+   * Gets a value that indicates whether the reader has more data to read.
+   */
   get hasNext() {
     return this._bytePos < this._view.byteLength;
   }
 
+  /**
+   * Reads the next byte in the buffer.
+   */
   readByte() {
     return this._view.getUint8(this._bytePos++);
   }
 
+  /**
+   * Reads the next byteLength bytes as a DataView.
+   * @param {int} byteLength - The number of bytes to make available in the DataView.
+   */
   readDataView(byteLength) {
     const view = new DataView(this._view.buffer, this._bytePos, byteLength);
     this._bytePos += byteLength;
